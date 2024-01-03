5fb91453c624975b51f03f441df9d153783ba250
Vishal Hirawat <hirawatt@pm.me>
2024-01-03 21:44:14 +0530

    added demo data for teams

--- pages/teams.py
+++ pages/teams.py
@@ -2,8 +2,18 @@ import streamlit as st
 import redis
 
 stream_name = "auction:jpls5"
-
-st.header("Team Auction Points")
-
 r = st.session_state.db
-st.write(r)
+
+#st.header("Team Auction Points")
+# FIXME: make this dynamic from DB
+team_list = ["Team 1", "Team 2", "Team 3", "Team 4"]
+team_points = [10000, 10000, 13000, 15000]
+team_max_bid = [i/20 for i in team_points]
+
+# Team points dashboard
+c1, c2, c3 = st.columns(3, gap="small")
+for team, points, max_bid in zip(team_list, team_points, team_max_bid):
+    c1.subheader(team)
+    c1.metric("Total Points", points)
+    c2.subheader("🛎")
+    c2.metric("Max Bid Allowed", int(max_bid))
