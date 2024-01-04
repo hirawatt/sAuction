7cd1ec7c3d69548938b014925538e94a81ba8ce1
Vishal Hirawat <hirawatt@pm.me>
2024-01-04 11:32:55 +0530

    updated db logic + added exception handling

--- pages/admin.py
+++ pages/admin.py
@@ -1,6 +1,7 @@
 import hmac
 import streamlit as st
 import pandas as pd
+import msgpack
 
 def check_password():
     """Returns `True` if the user had the correct password."""
@@ -30,14 +31,50 @@ if not check_password():
 
 # Admin Panel Access
 st.header("Admin Settings")
+try:
+    r = st.session_state.db
+except Exception as e:
+    print(e)
+    st.info("Contact Admin/Go to Dashboard & return to this page")
+    st.stop()
 
 # FIXME: make this data dynamic from db
-df = pd.DataFrame([["Team 1", "10000", "10000", "500", "0"], ["Team 2", "12000", "12000", "600", "0"]], columns=["Teams", "Starting Points", "Present Points", "Max Bid per Player", "Bonus Points"])
+team_list = ["Team 1", "Team 2", "Team 3", "Team 4"]
+team_points = [10000, 10000, 13000, 15000]
+bonus_list = [0, 0, 0, 0]
+# FIXME: calculate these
+team_max_bid = [i/20 for i in team_points]
+present_points = team_points
 
+data = {"team": team_list, "start_points": team_points, "bonus": bonus_list, "present_points": present_points, "max_bid_pp": team_max_bid}
+if 'df' not in st.session_state:
+    st.session_state.df = pd.DataFrame(data)
+
+df = st.session_state.df
 edited_df = st.data_editor(
     df,
-    disabled=["Starting Points", "Teams", "Max Bid per Player"],
+    disabled=["start_points", "team", "max_bid_pp"],
+    column_config={
+        "team": "Team Name",
+        "start_points": "Starting Points",
+        "bonus": st.column_config.NumberColumn(
+            "Bonus Points",
+            help="How much do you like this command (1-5)?",
+            min_value=0,
+            max_value=5000,
+            step=1000,
+            format="%d ⭐",
+            ),
+        "present_points": "Present Points",
+        "max_bid_pp": "Max Bid per Player",
+    },
+    num_rows="dynamic",
     hide_index=True,
     use_container_width=True)
 
-#st.write(edited_df)
+if st.button("Update Data"):
+    # store df in redis db
+    r.set("default_df", msgpack.packb(edited_df.to_dict(), use_bin_type=True))
+    df_retrieved = pd.DataFrame(msgpack.unpackb(r.get("default_df"), raw=False, strict_map_key=False))
+    with st.expander("Data"):
+        st.dataframe(df_retrieved, use_container_width=True)
--- pages/teams.py
+++ pages/teams.py
@@ -1,19 +1,32 @@
 import streamlit as st
 import redis
+import pandas as pd
+import msgpack
 
 stream_name = "auction:jpls5"
-#r = st.session_state.db
 
-#st.header("Team Auction Points")
-# FIXME: make this dynamic from DB
-team_list = ["Team 1", "Team 2", "Team 3", "Team 4"]
-team_points = [10000, 10000, 13000, 15000]
-team_max_bid = [i/20 for i in team_points]
+try:
+    #df = st.session_state.df
+    r = st.session_state.db
+except Exception as e:
+    print(e)
+    st.info("Enter Admin Password to continue")
+    st.stop()
 
-# Team points dashboard
-c1, c2, c3 = st.columns(3, gap="small")
-for team, points, max_bid in zip(team_list, team_points, team_max_bid):
-    c1.subheader(team)
-    c1.metric("Total Points", points)
-    c2.subheader("🛎")
-    c2.metric("Max Bid Allowed", int(max_bid))
+if st.button("Refresh", use_container_width=True):
+    # get df from redis db
+    df = pd.DataFrame(msgpack.unpackb(r.get("default_df"), raw=False, strict_map_key=False))
+    
+    team_list = df["team"].tolist()
+    team_points = df["present_points"].tolist()
+    team_max_bid = df["max_bid_pp"].tolist()
+
+    # Team points dashboard
+    c = st.columns(3, gap="small")
+    for team, points, max_bid in zip(team_list, team_points, team_max_bid):
+        c[0].subheader(team)
+        c[0].metric(":blue[Total Points]", points)
+        c[1].subheader("🛎")
+        c[1].metric(":blue[Max Bid Allowed]", int(max_bid))
+else:
+    st.info("Refresh to load data")
